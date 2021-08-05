5980514d789490ec0782addf0f90d7138e26af8b
dawon <lyumilove9@gmail.com>
2021-08-05 21:47:02 +0900

    footer 섹션 제작 및 css 수정, scroll 이벤트 제작

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.navbar_menu');
+const navLogo = document.querySelector('#navbar_logo');
 
 //모바일 메뉴
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
 
 menu.addEventListener('click', mobileMenu);
+
+//스크롤
+const highlightMenu = () => {
+    const element = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home');
+    const aboutMenu = document.querySelector('#about');
+    const servicesMenu = document.querySelector('#services');
+    let scrollPos = window.scrollY;
+
+    if(window.innerWidth > 960 && scrollPos < 600){
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if((element && window.innerWidth < 960 && scrollPos < 600) || element) {
+        element.classList.remove('highlight');
+    }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('.is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
